bee25a4a8fa72460206b097feb4983fb7c644ec9
Jarno Luucies <jarno.luucies@gmail.com>
2024-12-18 17:38:53 +0100

    Initial repo

---
+++ index.js
@@ -0,0 +1,36 @@
+// The main script for the extension
+// The following are examples of some basic extension functionality
+
+//You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
+import { extension_settings, getContext } from "../../../extensions.js";
+
+// Keep track of where your extension is located, name should match repo name
+const extensionName = "st-extension-example";
+const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
+const extensionSettings = extension_settings[extensionName];
+const defaultSettings = {};
+ 
+// Loads the extension settings if they exist, otherwise initializes them to the defaults.
+async function loadSettings() {
+  //Create the settings if they don't exist
+  extension_settings[extensionName] = extension_settings[extensionName] || {};
+  if (Object.keys(extension_settings[extensionName]).length === 0) {
+    Object.assign(extension_settings[extensionName], defaultSettings);
+  }
+
+  // Updating settings in the UI
+  $("#example_setting").prop("checked", extension_settings[extensionName].example_setting).trigger("input");
+}
+
+// This function is called when the extension is loaded
+jQuery(async () => {
+  // Load settings when starting things up (if you have any)
+  await loadSettings();
+
+  // This is an example of loading HTML from a file
+  // const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
+
+  // Test
+  console.log(getContext())
+
+});
